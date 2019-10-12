2dc4dcd007c1eef625cd3e07fd99935394606812
Rajat Verma <rajatverma5885045@gmail.com>
2019-10-13 02:11:21 +0530

    footer fixed

--- frontend/src/components/header-footer/footer.js
+++ frontend/src/components/header-footer/footer.js
@@ -1,16 +1,19 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+
 const Footer = () => {
   return (
     <div>
-      <footer className=" text-white d-none d-md-block text-center page-footer bottom-fixed">
+      <footer className=" text-white d-md-block text-center page-footer bottom-fixed">
+        <div>
         <span className="text-light font-weight-bolder">
-          &copy; Nibble Computer Society
+          DEVELOPED BY : <Link to="http://hackncs.com/"><span className="font-weight-normal">NIBBLE COMPUTER SOCIETY</span></Link>
         </span>
-      </footer>
-      <footer className=" text-white d-md-none text-center page-footer">
-        <span className="text-light font-weight-bolder">
-          &copy; Nibble Computer Society
+
+        <span className="text-light font-weight-bolder forum-link">
+          ALUMINI'S & FACULTY, VISIT : <Link to="https://www.facebook.com/nibblecomputersociety"><span className="font-weight-normal">FORUM FOR TREKKING</span></Link>
         </span>
+        </div>
       </footer>
     </div>
   );
