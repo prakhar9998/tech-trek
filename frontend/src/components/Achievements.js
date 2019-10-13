eda0db617c3af20ebc9e2f620b7f26ba9e0f08a0
kunalvishnoi <vishnoi.kunal0@gmail.com>
2019-10-14 01:22:16 +0530

    add badge

--- frontend/src/components/Achievements.js
+++ frontend/src/components/Achievements.js
@@ -11,7 +11,7 @@ class Achievements extends Component {
             src={
               badges.includes("1")
                 ? "https://i.ibb.co/gTQdLQY/Rex-Badge-2.png"
-                : ""
+                : "https://i.ibb.co/v4jS0Jq/Rex-Badge-Overlay-4.png"
             }
             alt="avatar01"
           />
@@ -23,10 +23,11 @@ class Achievements extends Component {
             src={
               badges.includes("2")
                 ? "https://i.ibb.co/C17CFfB/Rex-Badge-1.png"
-                : "https://cdna.artstation.com/p/assets/images/images/006/102/672/large/brice-laville-saint-martin-neytiri-01.jpg?1496079157"
+                : "https://i.ibb.co/1K5CZxD/Rex-Badge-Overlay-3.png"
             }
             alt="avatar01"
           />
+
           <p>Badge 2</p>
         </div>
         <div className="text-center">
@@ -35,10 +36,11 @@ class Achievements extends Component {
             src={
               badges.includes("3")
                 ? "https://i.ibb.co/pJdLL5L/Rex-Badge-3.png"
-                : "https://cdna.artstation.com/p/assets/images/images/006/102/672/large/brice-laville-saint-martin-neytiri-01.jpg?1496079157"
+                : "https://i.ibb.co/PmJ24rF/Rex-Badge-Overlay-2.png"
             }
             alt="avatar01"
           />
+
           <p>Badge 3</p>
         </div>
         <div className="text-center">
@@ -46,8 +48,8 @@ class Achievements extends Component {
             className="badge-image"
             src={
               badges.includes("4")
-                ? "https://i.ibb.co/m5BQr12/Rex-Badge4-1.png"
-                : "https://cdna.artstation.com/p/assets/images/images/006/102/672/large/brice-laville-saint-martin-neytiri-01.jpg?1496079157"
+                ? "https://i.ibb.co/9pBpNnn/Asset-7.png"
+                : "https://i.ibb.co/3FYKLqY/Rex-Badge-Overlay-5.png"
             }
             alt="avatar01"
           />
@@ -59,11 +61,12 @@ class Achievements extends Component {
             className="badge-image"
             src={
               badges.includes("5")
-                ? "https://i.ibb.co/9pBpNnn/Asset-7.png"
-                : "https://cdna.artstation.com/p/assets/images/images/006/102/672/large/brice-laville-saint-martin-neytiri-01.jpg?1496079157"
+                ? "https://i.ibb.co/m5BQr12/Rex-Badge4-1.png"
+                : "https://i.ibb.co/w6tSnFd/Rex-Badge-Overlay-1.png"
             }
             alt="avatar01"
           />
+
           <p>Badge 5</p>
         </div>
         <div className="text-center">
@@ -71,11 +74,12 @@ class Achievements extends Component {
             className="badge-image"
             src={
               badges.includes("6")
-                ? ""
-                : "https://cdna.artstation.com/p/assets/images/images/006/102/672/large/brice-laville-saint-martin-neytiri-01.jpg?1496079157"
+                ? "https://i.ibb.co/k5HV1fQ/Rex-Badge5.png"
+                : "https://i.ibb.co/k07Mmpy/Rex-Badge-Overlay-6.png"
             }
             alt="avatar01"
           />
+
           <p>Badge 6</p>
         </div>
       </div>
