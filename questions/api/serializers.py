0a6d480380e5267382a5537276e9ab57985bc41e
Prakhar Varshney <prakhar.varshney33@gmail.com>
2019-10-13 20:04:27 +0530

    update leaderboard api

--- questions/api/serializers.py
+++ questions/api/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 # from questions.models import Question
 from accounts.models import Player
+from badges.api.serializers import BadgesSerializer
+from django.db.models import Count
 
 class PlayerInfoSerializer(serializers.ModelSerializer):
     class Meta:
@@ -15,7 +17,12 @@ class PlayerInfoSerializer(serializers.ModelSerializer):
 
 class LeaderboardSerializer(serializers.BaseSerializer):
     def to_representation(self, obj):
+        queryset = obj.badges.annotate(total=Count('badge_type'))
+        badge_serializer = BadgesSerializer(queryset, many=True)
         return {
             'player_name': obj.username,
             'score': obj.score,
+            'email': obj.email,
+            'avatar_no': obj.avatar_no,
+            'badges': badge_serializer.data,
         }
