0615837d686d50e791b52e5adca12fe3b7e5dd19
Prakhar Varshney <prakhar.varshney33@gmail.com>
2019-10-14 07:27:32 +0530

    fix payment models

--- techtrek/settings.py
+++ techtrek/settings.py
@@ -76,7 +76,6 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'techtrek.middleware.TimeMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
