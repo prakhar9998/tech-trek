945da20b1955b2e19048d48e309d62db6937eea0
kunalvishnoi <vishnoi.kunal0@gmail.com>
2019-10-16 00:01:49 +0530

    change

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -46,14 +46,14 @@ class Dashboard extends Component {
   }
 
   componentDidMount() {
-    const time = new Date();
-    const hours = time.getHours();
-    const min = time.getMinutes();
-    const sec = time.getSeconds();
-    const TimeLeft = (23 - hours) * 3600 + (60 - min) * 60 + (60 - sec);
-    this.setState({
-      startTime: TimeLeft
-    });
+    // const time = new Date();
+    // const hours = time.getHours();
+    // const min = time.getMinutes();
+    // const sec = time.getSeconds();
+    // const TimeLeft = (23 - hours) * 3600 + (60 - min) * 60 + (60 - sec);
+    // // this.setState({
+    // //   startTime: TimeLeft
+    // // });
     const localtoken = localStorage.getItem("logintoken");
 
     fetch(`${process.env.REACT_APP_URL}/questions/`, {
